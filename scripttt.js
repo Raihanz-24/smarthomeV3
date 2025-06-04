6c18aea679bc0d28f7b8b10a1c13bfa7844a7f29
Raihan <ramaraihan559@gmail.com>
2025-06-05 00:14:58 +0700

    fixbutton

---
+++ scripttt.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCkdU7j55PWqYH5za7FftbJv4uFEpznjg4",
+    authDomain: "smarthomev-3.firebaseapp.com",
+    databaseURL: "https://smarthomev-3-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "smarthomev-3",
+    storageBucket: "smarthomev-3.appspot.com",
+    messagingSenderId: "375801588908",
+    appId: "1:375801588908:web:329cf3be86eafc77234309",
+    measurementId: "G-3R2RBQHG95"
+};
